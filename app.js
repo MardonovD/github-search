8b8451c2484d818460cfe198c4868085dd759d80
MardonovD <mardonovdoston69@gmail.com>
2022-04-05 10:17:28 +0500

    js created

--- app.js
+++ app.js
@@ -5,8 +5,23 @@ const inp = document.querySelector("#input");
 inp.addEventListener("keypress", ishla);
 
 function ishla(e) {
-  let inpVal = inp.value;
+  let username = inp.value;
+
   if (e.keyCode == 13) {
-    
+    console.log(username);
+
+    const urll = async function () {
+      const fetchh = await fetch(`https://api.github.com/users/${username}`);
+      const javob = await fetchh.json();
+
+      console.log(javob.updated_at);
+      console.log(javob);
+    };
+
+    urll();
   }
 }
+
+//  updated_at
+
+
