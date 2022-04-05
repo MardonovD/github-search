6307399928b59b322fd6ec70bd9ddbe8077d60b1
MardonovD <mardonovdoston69@gmail.com>
2022-04-05 09:06:39 +0500

    search create

---
+++ app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const inp = document.querySelector("#input");
+
+inp.addEventListener("keypress", ishla);
+
+function ishla(e) {
+  let inpVal = inp.value;
+  if (e.keyCode == 13) {
+    
+  }
+}
